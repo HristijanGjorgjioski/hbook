e5276c6d84f3849e6085a11be25c4e61af132322
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-27 06:32:25 +0200

    Trying different solutions for importing files

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
@@ -7,11 +7,28 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        if(args.path === 'index.js') {
+        if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if(args.path === 'tiny-test-pkg') {
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' }
         }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -21,14 +38,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './tiny-test-pkg';
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
-        } 
+        }
 
         const { data } = await axios.get(args.path);
-        console.log(data);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
