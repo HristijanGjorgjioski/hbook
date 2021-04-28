e97a1cf293edc4bf3eb2b270c5d197570b58c04b
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-28 21:43:30 +0200

    Adding some options to MonacoEditor component with help of monaco-editor documentation

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,21 @@
-import Editor from "@monaco-editor/react";
+import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <Editor language="javascript" height="400px" />
+    return (
+        <MonacoEditor
+            theme="vs-dark"
+            language="javascript"
+            height="400px"
+            options={{
+                wordWrap: 'on',
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16
+            }}
+        />
+    );
 };
 
 export default CodeEditor;
