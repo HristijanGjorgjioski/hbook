0ac28d3316d8f8253a5b9c0996fb7b820d7d00a5
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-28 13:44:40 +0200

    Trying to execute code in iframe html element

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -35,12 +36,25 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
-    <script>${code}</script>
-  `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Document</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          eval(event.data)
+        }, false)
+      </script>
+    </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -52,7 +66,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html} title={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} title={html} />
     </div>
   );
 };
