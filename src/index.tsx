260aae301483aac54b5a3084d526e0bd8d132f5c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-29 07:15:33 +0200

    Created CodeCell component and move all logic for editor there, now i am trying to allow user to have multiple editors at once

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler/index';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="// Creator: Hristijan Gjorgjioski" 
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
