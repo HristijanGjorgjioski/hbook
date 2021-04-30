bdd5cdde722360d8aac1f404cac1b92b4641f269
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-30 06:40:47 +0200

    Added debouncing logic and cleared the timeout

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -29,7 +29,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return () => {
       window.removeEventListener('resize', listener);
     }
-  }, [])
+  }, [width])
 
   if (direction === 'horizontal') {
     resizableProps = {
