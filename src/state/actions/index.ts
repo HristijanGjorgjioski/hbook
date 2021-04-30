252ba257d022d10c1e8d992944891645e0a51b17
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-30 15:50:26 +0200

    Created separate file to define Cell types

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,30 +1,33 @@
-import { ActionType } from '../action-types/index'
+import { ActionType } from '../action-types/index';
+import { CellTypes } from '../cell';
 
 interface MoveCellAction {
     type: ActionType.MOVE_CELL;
     payload: {
         id: string;
         direction: 'up' | 'down';
-    }
-}
+    };
+};
 
 interface DeleteCellAction {
     type: ActionType.DELETE_CELL;
     payload: string;
-}
+};
 
 interface InsertCellBefore {
     type: ActionType.INSERT_CELL_BEFORE;
     payload: {
         id: string;
-        type: 'code' | 'text';
-    }
-}
+        type: CellTypes;
+    };
+};
 
 interface UpdateCellAction {
     type: ActionType.UPDATE_CELL;
     payload: {
         id: string;
         content: string;
-    }
-}
+    };
+};
+
+export type Action = MoveCellAction | DeleteCellAction | InsertCellBefore | UpdateCellAction;
